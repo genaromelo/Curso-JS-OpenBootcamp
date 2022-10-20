6b0462fc730017c45a769531fe2072154c2c5fef
GenaroMelo <genaromelo5@gmail.com>
2022-10-20 17:23:12 -0300

    Ejercicios 1 y 2

---
+++ Ejercicios/Ejercicio_2.js
@@ -0,0 +1,15 @@
+// LISTA DE DATOS PERSONALES
+
+const listaDeDatos = {
+    nombre: "Genaro Melo",
+    edad: 26,
+    desarrollador: true,
+    fechaDeNacimiento: new Date(1996, 0, 25),
+    libroFavorito: {
+        titulo: "La conjura de los necios",
+        autor: "John Kennedy Toole",
+        fecha: 1980,
+        url: "https://es.wikipedia.org/wiki/La_conjura_de_los_necios",
+    }
+}
+
